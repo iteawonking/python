cab10699d2a6fc28d762d10e3aa6d989d5d24b60
iteawonking <167491029+iteawonking@users.noreply.github.com>
2024-04-19 15:04:59 +0900

    Add files via upload

---
+++ Quiz on List.py
@@ -0,0 +1,24 @@
+#Quiz on List 김우준 2024133519
+
+#Number1
+a_list = [1, 'hello', [1, 2, 3] , True]
+a_list
+
+[1, 'hello', [1, 2, 3], True]
+
+#Number2
+a_list[1]
+
+hello
+
+#Number3
+a_list[1:4]
+['hello', [1, 2, 3], True]
+
+
+#Number4
+A = [1, 'a'] 
+B = [2, 1, 'd']
+A + B
+
+[1, 'a', 2, 1, 'd']
