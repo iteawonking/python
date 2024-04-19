c2c47df6e6967fdef443960882eaa9e6cfefc594
iteawonking <167491029+iteawonking@users.noreply.github.com>
2024-04-19 14:58:41 +0900

    Add files via upload

---
+++ Quiz on Tuples.py
@@ -0,0 +1,34 @@
+#Quiz on Tuples김우준 2024133519
+
+genres_tuple = ("pop", "rock", "soul", "hard rock", "soft rock", \
+                "R&B", "progressive rock", "disco") 
+
+
+#Number1
+len(genres_tuple)
+8
+
+#Number2
+genres_tuple[3]
+hard rock
+
+#Number3
+genres_tuple[3:6]
+('hard rock', 'soft rock', 'R&B')
+
+
+#Number4
+genres_tuple[0:2]
+('pop', 'rock')
+
+
+#Number5
+genres_tuple.index("disco")
+ 7
+
+#Number6
+ C_tuple = (-5, 1, -3)
+C_list = sorted(C_tuple)
+C_list
+
+[-5, -3, 1]
