82da86818ef38a71c98dced1fc5e4e641c633d23
iteawonking <167491029+iteawonking@users.noreply.github.com>
2024-04-19 14:37:22 +0900

    Add files via upload

---
+++ Exercise Strings1.py
@@ -0,0 +1,51 @@
+#Exercise Strings1 김우준 2024133519
+
+
+#Number1
+A="1"
+
+#Number2
+B="2"
+
+#Number3
+A="1"
+B="2"
+C=A+B
+print(c)
+12
+
+#Number4
+D = "ABCDEFG"
+print(D[:3])
+ABC
+
+#Number5
+E = 'clocrkr1e1c1t'
+print(E[::2])
+correct
+
+#Number6
+print("\\")
+\
+  
+#Number7
+F = "You are wrong"
+F.upper()
+YOU ARE WRONG
+
+#Number8
+G = "Mary had a little lamb Little lamb, little lamb Mary had a little lamb \
+Its fleece was white as snow And everywhere that Mary went Mary went, Mary went \
+Everywhere that Mary went The lamb was sure to go"
+
+G.find("snow")
+95
+
+#Number9
+
+G = "Mary had a little lamb Little lamb, little lamb Mary had a little lamb \
+Its fleece was white as snow And everywhere that Mary went Mary went, Mary went \
+Everywhere that Mary went The lamb was sure to go"
+
+G.replace("Mary", "Bob")
+Bob had a little lamb Little lamb, little lamb Bob had a little lamb Its fleece was white as snow And everywhere that Bob went Bob went, Bob went Everywhere that Bob went The lamb was sure to go
