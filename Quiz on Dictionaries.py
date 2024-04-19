97bf2d7466bcd34255df02338f852930c61d4b72
iteawonking <167491029+iteawonking@users.noreply.github.com>
2024-04-19 15:16:19 +0900

    Add files via upload

---
+++ Quiz on Dictionaries.py
@@ -0,0 +1,34 @@
+# Quiz on Dictionaries 김우준 2024133519
+
+#Number1
+
+soundtrack_dic = {"The Bodyguard":"1992", "Saturday Night Fever":"1977"}
+#a
+soundtrack_dic.keys()
+
+dict_keys(['The Bodyguard', 'Saturday Night Fever'])
+
+#b
+soundtrack_dic.values()
+
+dict_values(['1992', '1977'])
+
+#Number2
+
+#a
+album_sales_dict = {"The Bodyguard":50, "Back in Black":50, "Thriller":65}
+
+#b
+album_sales_dict["Thriller"]
+
+65
+
+#c
+album_sales_dict.keys()
+
+dict_keys(['The Bodyguard', 'Back in Black', 'Thriller'])
+
+#d
+album_sales_dict.values()
+
+dict_values([50, 50, 65])
