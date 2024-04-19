2ead4cb383c716a6fea3ef74a2c842b535c99c14
iteawonking <167491029+iteawonking@users.noreply.github.com>
2024-04-19 16:10:45 +0900

    Add files via upload

---
+++ Quiz on Functions.py
@@ -0,0 +1,22 @@
+#Quiz on Functions 김우준 2024133519
+
+#Number1
+def div(a, b):
+    return(a/b)
+div(1,2)
+
+0.5
+
+#Number2
+def con(a, b):
+    return(a + b)
+
+con(1, 2)
+
+3
+
+#Number3
+con(['a', 1], ['b', 1])
+con(1, 2)
+
+3
