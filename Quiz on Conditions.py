db824024b6c3dcb378d540fe9749b46641edd905
iteawonking <167491029+iteawonking@users.noreply.github.com>
2024-04-19 15:25:06 +0900

    Add files via upload

---
+++ Quiz on Conditions.py
@@ -0,0 +1,25 @@
+#Quiz on Conditions 김우준 2024133519
+
+#Number1
+rating = 8.5
+if rating > 8:
+    print ("This album is Amazing!")
+
+This album is Amazing!
+
+#Number2
+rating = 7
+if rating > 8:
+    print ("this album is amazing")
+else:
+    print ("this album is")
+
+this album is ok
+
+#Number3
+album_year = 1991
+
+if album_year < 1980 or album_year == 1991 or album_year == 1993:
+    print ("this album came out already")
+
+this album came out already
